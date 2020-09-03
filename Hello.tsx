c092ae28a552e1c635ba2d55fb975ae705af7b53
cafesanu <cafesanu@gmail.com>
2020-09-03 13:27:15 -0400

    1 file modified

--- Hello.tsx
+++ Hello.tsx
@@ -3,18 +3,31 @@ import { Line } from '@nivo/line';
 import { random } from 'faker';
 
 const random0to60 = () => random.number({ min: 0, max: 60 });
+const generateData = (num = 12) => {
+  return [...Array(4)].map((_, i) => ({
+    id: `data${i + 1}`,
+    data: [...Array(num)].map((_, j) => ({ x: `${j + 1}`, y: random0to60() })),
+  }));
+};
+const generateDacta = (num = 12) => {
+  return [
+    {
+      id: 'data1',
+      data: [...Array(num)]
+        .fill(1)
+        .map((_, j) => {
+          return { x: `${j+1}`, y: random0to60() }
+        })
+    }
+  ];
+}
 
-const data = [...Array(3)]
-  .fill(1)
-  .map((_, j) => {
-    return { x: `${j+1}`, y: random0to60() }
-  });
 
 export const Hello = () => {
   return (<Line
     width={900}
     height={400}
     margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
-    data={data}
+    data={generateDacta()}
   />)
 };
