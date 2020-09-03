06d636208e332eb6381c097736653bcb334facd1
cafesanu <cafesanu@gmail.com>
2020-09-03 13:43:10 -0400

    1 file modified

--- Hello.tsx
+++ Hello.tsx
@@ -3,6 +3,8 @@ import { Line } from '@nivo/line';
 import { random } from 'faker';
 
 const random0to60 = () => random.number({ min: 0, max: 60 });
+console.log(Math.random() +1)
+
 const generateData = (num = 12) => {
   return [...Array(4)].map((_, i) => ({
     id: `data${i + 1}`,
@@ -10,6 +12,9 @@ const generateData = (num = 12) => {
   }));
 };
 const generateDacta = (num = 12) => {
+  const points = [...Array(num)]
+        .fill(1)
+        .map(random0to60)
   return [...Array(4)]
     .fill(1)
     .map((_, i) => ({
@@ -17,7 +22,7 @@ const generateDacta = (num = 12) => {
       data: [...Array(num)]
         .fill(1)
         .map((_, j) => {
-          return { x: `${j+1}`, y: random0to60() }
+          return { x: `${j+1}`, y: points[j] * random0to60()}
         })
     }));
 }
@@ -25,38 +30,55 @@ const generateDacta = (num = 12) => {
 
 export const Hello = () => {
   const [data, setData] = useState(generateDacta());
+  const [stacked, setStacked] = useState(false);
 
   return (
     <div>
       <button
-          style={{ marginRight: '5px' }}
-          onClick={() => {
-            setData(generateDacta(12));
-          }}
-        >
-          12 points (for months)
-        </button>
-        <button
-          style={{ marginRight: '5px' }}
-          onClick={() => {
-            setData(generateDacta(24));
-          }}
-        >
-          24 points (for day)
-        </button>
-        <button
-          style={{ marginRight: '5px' }}
-          onClick={() => {
-            setData(generateDacta(31));
-          }}
-        >
-          31 points (for month)
-        </button>
+        style={{ marginRight: '5px' }}
+        onClick={() => {
+          setStacked((val) => !val);
+        }}
+      >
+        Stacked? ({stacked ? "true": "false"})
+      </button>
+      <button
+        style={{ marginRight: '5px' }}
+        onClick={() => {
+          setData(generateDacta(12));
+        }}
+      >
+        12 points (for months)
+      </button>
+      <button
+        style={{ marginRight: '5px' }}
+        onClick={() => {
+          setData(generateDacta(24));
+        }}
+      >
+        24 points (for day)
+      </button>
+      <button
+        style={{ marginRight: '5px' }}
+        onClick={() => {
+          setData(generateDacta(31));
+        }}
+      >
+        31 points (for month)
+      </button>
       <Line
         width={900}
         height={400}
         margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
         data={data}
+        colors={['#45227B', '#6E36C5', '#8A43F6', '#A06DEE']}
+        enableSlices="x"
+        enableArea={true}
+        yScale={{
+          type: 'linear',
+          stacked: stacked,
+        }}
+        areaOpacity={0.8}
       />
     </div>
   )
