3857036c3795f89ec25ee67cc67d35207016b528
cafesanu <cafesanu@gmail.com>
2020-09-03 13:31:27 -0400

    1 file modified

--- Hello.tsx
+++ Hello.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Line } from '@nivo/line';
 import { random } from 'faker';
 
@@ -10,24 +10,54 @@ const generateData = (num = 12) => {
   }));
 };
 const generateDacta = (num = 12) => {
-  return [
-    {
-      id: 'data1',
+  return [...Array(4)]
+    .fill(1)
+    .map((_, i) => ({
+      id: `data${i + 1}`,
       data: [...Array(num)]
         .fill(1)
         .map((_, j) => {
           return { x: `${j+1}`, y: random0to60() }
         })
-    }
-  ];
+    }));
 }
 
 
 export const Hello = () => {
-  return (<Line
-    width={900}
-    height={400}
-    margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
-    data={generateDacta()}
-  />)
+  const [data, setData] = useState(generateDacta());
+
+  return (
+    <div>
+      <button
+          style={{ marginRight: '5px' }}
+          onClick={() => {
+            setData(generateDacta(12));
+          }}
+        >
+          12 points (for months)
+        </button>
+        <button
+          style={{ marginRight: '5px' }}
+          onClick={() => {
+            setData(generateDacta(24));
+          }}
+        >
+          24 points (for day)
+        </button>
+        <button
+          style={{ marginRight: '5px' }}
+          onClick={() => {
+            setData(generateDacta(31));
+          }}
+        >
+          31 points (for month)
+        </button>
+      <Line
+        width={900}
+        height={400}
+        margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+        data={data}
+      />
+    </div>
+  )
 };
